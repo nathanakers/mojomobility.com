0a6f837c1f13fadc775b55d693dfc65665d4063a
Nathan Akers <ncakers@gmail.com>
2022-05-20 16:14:51 -0700

    insert HOME page content

--- home.php
+++ home.php
@@ -2,35 +2,16 @@
 // Get absolute path to find our includes files
 // NOTE - we can't get our @var $base-url until we call functions
 global $base_dir;
-$base_dir = rtrim( dirname( realpath( __FILE__ ) ), '/' );
+$base_dir = rtrim(dirname(realpath(__FILE__)), '/');
 
 // Required as the first instance for each page, and for editing using Couch CMS, REF = http://www.couchcms.com/
 // require_once( $base_dir . '/admin-cms/cms.php' );
 // Include Functions & Headers
-include( $base_dir . '/includes/functions.php' );
-include( $base_dir . '/includes/html-app-header.php' );
-include( $base_dir . '/includes/html-content-header.php' );
+include($base_dir . '/includes/functions.php');
+include($base_dir . '/includes/html-app-header.php');
+include($base_dir . '/includes/html-content-header.php');
 ?>
 <!-- // BEGIN PAGE CONTENT (inside #main div) -->
-
-
-<cms:template title="Home Page Content" clonable="0" executable="1" order="10">
-    <cms:editable name="group_sliders" label="Slider Content" type="group" order="1" />
-        <cms:editable name="slider_desk"       label="Desk Slider Text"           group="group_sliders" type="text"     order="1" required="1" />
-        <cms:editable name="slider_bed"        label="Bed Slider Text"            group="group_sliders" type="text"     order="2" required="1" />
-        <cms:editable name="slider_cafe"       label="Cafe Slider Text"           group="group_sliders" type="text"     order="3" required="1" />
-        <cms:editable name="slider_car"        label="Car Slider Text"            group="group_sliders" type="text"     order="4" required="1" />
-
-    <cms:editable name="group_body" label="Main Content" type="group" order="3" />
-        <cms:editable name="intro_headline"    label="Intro Headline"             group="group_body"    type="text"     order="1" required="1" />
-        <cms:editable name="body_content"      label="Body Content"               group="group_body"    type="richtext" order="2" required="1" />
-        <cms:editable name="wwd_header"        label="What We Do Header"          group="group_body"    type="text"     order="3"              desc="Default is 'What We Do'" >What We Do</cms:editable>
-        <cms:editable name="what_we_do"        label="What We Do"                 group="group_body"    type="richtext" order="4" required="1" />
-        <cms:editable name="pov_header"        label="Point of View Header"       group="group_body"    type="text"     order="5"              desc="Default is 'Our Point of View" >Our Point of View</cms:editable>
-        <cms:editable name="point_of_view"     label="Our Point of View"          group="group_body"    type="richtext" order="6" required="1" />
-        <cms:editable name="button_technology" label="Button links to Technology" group="group_body"    type="text"     order="7" required="1" desc="This text is followed by  →" />
-        <cms:editable name="button_solutions"  label="Button links to Solutions"  group="group_body"    type="text"     order="8" required="1" desc="This text is followed by  →" />
-</cms:template>
 
 
 <!-- Content Slider -->
@@ -52,7 +33,9 @@ include( $base_dir . '/includes/html-content-header.php' );
                     <div class="container">
                         <div class="row">
                             <div class="span6 offset8">
-                                <p class="slider-headline"><span><cms:show slider_desk /></span></p>
+                                <p class="slider-headline">
+                                    <span>Mojo’s position free multi device charging enables intuitive, hassle-free charging in any environment</span>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -64,7 +47,9 @@ include( $base_dir . '/includes/html-content-header.php' );
                     <div class="container">
                         <div class="row">
                             <div class="span12 offset2">
-                                <p class="slider-headline"><span><cms:show slider_bed /></span></p>
+                                <p class="slider-headline">
+                                    <span>Mojo’s technology enables wireless charging of multiple products simultaneously</span>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -76,7 +61,9 @@ include( $base_dir . '/includes/html-content-header.php' );
                     <div class="container">
                         <div class="row">
                             <div class="span9 offset3">
-                                <p class="slider-headline"><span><cms:show slider_cafe /></span></p>
+                                <p class="slider-headline">
+                                    <span>Mojo’s position-free wireless charging, and ability to charge through surfaces, allows easy integration into public places</span>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -88,7 +75,9 @@ include( $base_dir . '/includes/html-content-header.php' );
                     <div class="container">
                         <div class="row">
                             <div class="span9 offset2">
-                                <p class="slider-headline"><span><cms:show slider_car /></span></p>
+                                <p class="slider-headline">
+                                    <span>Mojo technology enables the integration of easy to use, distraction-free wireless charging solutions for automotive applications</span>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -108,25 +97,34 @@ include( $base_dir . '/includes/html-content-header.php' );
         <div class="row">
             <div class="span12 offset2">
 
-                <h1><cms:show intro_headline /></h1>
+                <h1>Mojo Mobility partners with OEMs to design and develop unique, cutting-edge products that incorporate Mojo’s patented wireless charging technologies into a variety of different industries and applications—from mobile devices, to industrial and medical devices, to electric vehicles.</h1>
                 <p class="icons">
                     <i class="icon-mobile-phone"></i>
                     <i class="icon-home"></i>
                     <i class="icon-dashboard"></i>
                 </p>
-                <cms:show body_content />
+                <p>Established in 2005, Mojo Mobility has been at the forefront of the development of wireless power technologies that allow consumers to conveniently charge and power their devices. Mojo Mobility has partnered with a number of OEMs to create unique, advanced solutions for wireless charging for many different products. Mojo’s unique proprietary Near Field Power® technology allows position-free charging and powering of multiple devices simultaneously. Near Field Power® places Mojo Mobility several generations ahead of their competitors by providing unsurpassed performance, cost, efficient power transfer and a simplified user experience.</p>
+                <p>Mojo has also partnered with OEMs to enhance the automotive experience in several ways. Mojo incorporates its wireless charging technology into various surfaces in the car, creating a safer and more convenient alternative to cord-charging. Mojo Mobility is also transforming the electric vehicle consumer experience by enabling high-power, efficient wireless charging of the vehicle. Mojo is eliminating the hassle of plug-in charging by enabling a user-friendly, ubiquitous method of charging electric vehicles wirelessly.</p>
                 <hr>
 
                 <div class="row">
                     <div class="span6">
-                        <h2><cms:show wwd_header /></h2>
-                        <cms:show what_we_do />
-                        <p><a class="btn btn-large btn-inverse btn-mojoblue btn-mojostyleflat" href="technology"><cms:show button_technology />&nbsp;→</a></p>
+                        <h2>What We Do</h2>
+                        <p>Mojo Mobility delivers position-free wireless charging technology to OEMs, which enables multiple devices to charge simultaneously without the restriction of exact positioning of the transmitter and receiver. Traditional wireless charging has been limited by the necessity of precise alignment of the transmitter and receiver. We partner with OEMs worldwide to provide cutting-edge, production-ready wireless charging solutions. These solutions are industry proven and provide tremendous advantages over existing wireless charging technology. Our solutions are user-friendly, highly efficient, extremely safe, and meet all regulatory guidelines.</p>
+                        <p>
+                            <a class="btn btn-large btn-inverse btn-mojoblue btn-mojostyleflat" href="/technology">
+                                Explore Our Technology&nbsp;→
+                            </a>
+                        </p>
                     </div>
                     <div class="span6">
-                        <h2><cms:show pov_header /></h2>
-                        <cms:show point_of_view />
-                        <p><a class="btn btn-large btn-inverse btn-mojoblue btn-mojostyleflat" href="solutions"><cms:show button_solutions />&nbsp;→</a></p>
+                        <h2>OUR POINT OF VIEW</h2>
+                        <p>We believe wireless charging is the future of a world that is becoming truly mobile. No longer will consumers be restricted by an array of cords and chargers to charge their mobile devices or by the cord of an electric vehicle charging station. Our technology solutions allow designers of powered products to re-think and re-shape their design ideas to free their customers of the constraints of wired charging.</p>
+                        <p>
+                            <a class="btn btn-large btn-inverse btn-mojoblue btn-mojostyleflat" href="/solutions">
+                                View Our Solutions&nbsp;→
+                            </a>
+                        </p>
                     </div>
                 </div>
                 <hr>
@@ -140,8 +138,8 @@ include( $base_dir . '/includes/html-content-header.php' );
 <!-- // END PAGE CONTENT -->
 <?php
 // Include Functions & Headers
-include( $base_dir . '/includes/html-content-footer.php' );
-include( $base_dir . '/includes/html-app-footer.php' );
+include($base_dir . '/includes/html-content-footer.php');
+include($base_dir . '/includes/html-app-footer.php');
 
 // Required by Couch CMS, required at end of each page.
 // COUCH::invoke();
